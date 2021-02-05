68a1aadb58bf69f8690d22713160f7b13a86d2ed
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-02-05 19:08:47 +0530

    Update Template.cpp

--- TC/Template.cpp
+++ TC/Template.cpp
@@ -1,33 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-void solve();
-int main()
-{
-	ios_base::sync_with_stdio(false); cin.tie(NULL);
+class Class Name {
+public:
+Return Type Method Name(Arg Types){
 
-#ifndef ONLINE_JUDGE
-	freopen("input1.txt", "r", stdin);
-	freopen("error1.txt", "w", stderr);
-	freopen("output1.txt", "w", stdout);
-#endif
 
-	int t ; cin >> t;
-	while (t--)
-	{
-		solve();
-		cout << "\n";
-	}
-
-	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
-	return 0;
+	return value;
 }
-void solve()
-
-{
-	
-
-
-}
-
-
+};
