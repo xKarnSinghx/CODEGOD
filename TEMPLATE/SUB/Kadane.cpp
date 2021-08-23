ab23c8f281ab6954717a756f469c7c95498ddb63
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-08-23 22:20:52 +0530

    Create Kadane.cpp

---
+++ TEMPLATE/SUB/Kadane.cpp
@@ -0,0 +1,20 @@
+ll kadanesAlgo(vll ar)
+{
+	ll n = ar.size();
+
+	ll currMax = 0;
+	ll mx = INT_MIN;
+
+	for (ll i = 0; i < n; i++)
+	{
+		currMax += ar[i];
+
+		if (currMax <= ar[i])
+		{
+			currMax = ar[i];
+		}
+
+		mx = max(currMax, mx);
+	}
+	return mx;
+}
