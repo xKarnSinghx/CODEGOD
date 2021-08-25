dcf04c952eb3b5014d285080608b2e7843b00ff1
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-08-25 22:26:26 +0530

    Create PrimeFactor.cpp

---
+++ TEMPLATE/MATHS/PrimeFactor.cpp
@@ -0,0 +1,16 @@
+ll primeFactors(ll n)
+{
+	vll ar;
+	ll i = 2;
+	while (n > 0)
+	{
+		if (i > n) break;
+		if (n % i == 0)
+		{
+			ar.pb(i);
+			while (n > 0 and n % i == 0) n = n / i;
+		}
+		i++;
+	}
+	return ar.size();
+}
